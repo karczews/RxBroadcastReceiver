c0db03192ea5d4f2266e63964221db4a151bdf4e
Krzysztof Karczewski <krzysztof.karczewski@tomtom.com>
2017-07-25 22:11:53 +0200

    RxBroadcastReceivers javadocs. Add mvn publish script with initial configuration.

--- library/src/main/java/com/github/karczews/rxbroadcastreceiver/RxBroadcastReceivers.java
+++ library/src/main/java/com/github/karczews/rxbroadcastreceiver/RxBroadcastReceivers.java
@@ -8,11 +8,23 @@ import android.support.annotation.NonNull;
 
 import io.reactivex.Observable;
 
+/**
+ * Utility class containing static factory methods creating Observable
+ * instances wrapping {@link android.content.BroadcastReceiver}s
+ */
 public class RxBroadcastReceivers {
 
+    /**
+     * Creates Observable that will register {@link android.content.BroadcastReceiver} for provided
+     * {@link IntentFilter} when subscribed to. Observable will emit received broadcast as data {@link Intent}
+     *
+     * @param context used to register broadcast receiver to.
+     * @param filter {@link IntentFilter} used to select Intent broadcast to be received.
+     */
     @NonNull
     @CheckResult
-    public static Observable<Intent> fromIntentFilter(Context context, IntentFilter filter) {
+    public static Observable<Intent> fromIntentFilter(@NonNull final Context context,
+                                                      @NonNull final IntentFilter filter) {
         return new RxBroadcastReceiver(context, filter);
     }
 }
