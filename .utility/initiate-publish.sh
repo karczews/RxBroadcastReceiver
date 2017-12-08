b7daf9ebdc9cab92c7c600f71a6142e24bd236ed
Krzysztof Karczewski <krzysztof.karczewski@tomtom.com>
2017-12-08 10:59:40 +0100

    Adding auto snapshot publishing

---
+++ .utility/initiate-publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This script initiates the Gradle publishing task when pushes to master occur.
+# NOTE: Travis-CI can only publish SNAPSHOT versions. To release a version, you need
+#       to use the internal Jenkins job.
+
+echo Repo: $TRAVIS_REPO_SLUG Pull Request: $TRAVIS_PULL_REQUEST Branch: $TRAVIS_BRANCH
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+  export GIT_COUNT=`git rev-list HEAD --count`
+  echo "Starting publish to Sonatype...  $GIT_COUNT ${SONATYPE_USERNAME} ${SONATYPE_PASSWORD}"
+
+  ./gradlew uploadArchives -PNEXUS_USERNAME=${SONATYPE_USERNAME} -PNEXUS_PASSWORD=${SONATYPE_PASSWORD}
+  RETVAL=$?
+
+  if [ $RETVAL -eq 0 ]; then
+    echo 'Completed publish!'
+  else
+    echo 'Publish failed.'
+    return 1
+  fi
+
+fi
